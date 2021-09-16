63c64e4c93c7342b12cf0946a667fe555a26c6c5
iiropa <iiropaajanen2004@gmail.com>
2021-09-16 14:09:58 +0300

    added comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for (int x = 1; x <= 20; x++) {
+        for (int x = 1; x <= 20; x++) { //loop goes through values 1-20
             System.out.print(sumUpTo(x) + " ");
         }
 
     }
-    static int sumUpTo (int n) {
+    static int sumUpTo (int n) { //the core function
         return n * ( n + 1 )/2;
     }
 
